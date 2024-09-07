071598e65fba907dbef6070ee279f5f1cd0d24e2
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-09-07 15:06:02 +0300

    add some React styles 😍

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,42 +1,74 @@
 import React from "react";
+import AddIcon from '@mui/icons-material/Add';
+import { Fab,Zoom,Collapse } from "@mui/material";
 
 function CreateArea(props) {
-    const [title, setTitle] = React.useState("");
-    const [content, setContent] = React.useState("");
-    
-    function handleChange(event) {
-        const {name, value} = event.target;
-        
-        if (name === "title") {
-            setTitle(value);
-        } else if (name === "content") {
-            setContent(value);
-        }
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [isClicked, setIsClicked] = React.useState(false);
+  const [typingTimeout, setTypingTimeout] = React.useState(null);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    if (name === "title") {
+      setTitle(value);
+    } else if (name === "content") {
+      setContent(value);
     }
 
-    function onSubmit(event) {
-        props.onAdd({title, content});
-        setTitle("");
-        setContent("");
-        event.preventDefault();
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
     }
 
+    const newTimeout = setTimeout(() => {
+      setIsClicked(false);
+    }, 5000); 
 
+    setTypingTimeout(newTimeout);
+  }
+
+  function onSubmit(event) {
+    props.onAdd({ title, content });
+    setTitle("");
+    setContent("");
+    event.preventDefault();
+  }
+
+  function handleonClick() {
+    setIsClicked(true);
+
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+  }
 
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title"
-        onChange= {handleChange}
-        value={title}
+      <form className="create-note">
+        <Collapse in={isClicked} timeout="auto">
+          <input
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={title}
+          />
+        </Collapse>
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows={isClicked ? 3 : 1}
+          onChange={handleChange}
+          value={content}
+          onClick={handleonClick}
         />
-        <textarea name="content" placeholder="Take a note..." rows="3" 
-        onChange={handleChange}
-        value={content}
-        />
-        <button
-        onClick={onSubmit}
-        >Add</button>
+        <Collapse in={isClicked} timeout="auto">
+        <Zoom in={isClicked}>
+          <Fab onClick={onSubmit}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+        </Collapse>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import EditNoteIcon from '@mui/icons-material/EditNote';
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1> 
+        <EditNoteIcon className="edit-icon" fontSize="large" />  
+        Keeper
+      </h1>
     </header>
   );
 }
